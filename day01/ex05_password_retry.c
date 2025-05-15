c645e71e8530a50158af6b1dda96635af1199c63
Th3Hacker <hamza.annane.official@gmail.com>
2025-05-15 14:30:46 +0100

    day01 1337 piscine prep

---
+++ day01/ex05_password_retry.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex05_password_retry.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: 0xHamSec <0xHamSec@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15  by 0xHamSec                 #+#    #+#             */
+/*   Updated: 2025/05/15  by 0xHamSec                ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Ask user to enter the correct password. Repeat until correct using do while.
+** Example password: 1337
+*/
