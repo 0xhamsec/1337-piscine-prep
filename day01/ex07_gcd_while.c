017a9787333101f92ec9b9793c0845ae3551e899
Th3Hacker <hamza.annane.official@gmail.com>
2025-05-16 12:27:16 +0100

    BCD Euclidean algorithm

--- day01/ex07_gcd_while.c
+++ day01/ex07_gcd_while.c
@@ -13,3 +13,39 @@
 /*
 ** Ask for two integers. Find their GCD using Euclidean algorithm in a while loop.
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2;
+
+    printf("Enter two integers: ");
+    scanf("%d%d", &n1, &n2);
+
+    // التعامل مع القيم السالبة بتحويلها إلى موجبة
+    if (n1 < 0) n1 = -n1; // 
+    if (n2 < 0) n2 = -n2;
+
+    // طباعة الأعداد الأصلية بعد المعالجة
+    printf("Calculating GCD of %d and %d...\n", n1, n2);
+
+    // التأكد أن n1 ≥ n2
+    if (n2 > n1) {
+        int temp = n1;
+        n1 = n2;
+        n2 = temp;
+    }
+
+    // Euclidean algorithm
+    while (n2 != 0)
+    {
+        int temp = n1 % n2;
+        n1 = n2;
+        n2 = temp;
+    }
+
+    printf("The GCD = %d\n", n1);
+
+    return 0;
+}
