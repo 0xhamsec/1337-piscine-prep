4be555d04806ceebe7441939160db95a9789a8fb
Th3Hacker <hamza.annane.official@gmail.com>
2025-05-16 20:43:12 +0100

    count down  function

--- day02/03_ft_count_down.c
+++ day02/03_ft_count_down.c
@@ -12,3 +12,30 @@
 /*     This function prints numbers from n down to 0 using only write.       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+void    print_number(int n)
+{
+    if (n > 9)
+    {
+        print_number(n / 10);
+    }
+
+    char digit = (n % 10) + 48;
+    write(1,&digit,1);
+}
+
+void    ft_count_down(int n)
+{
+    while (n >= 0)
+    {
+        print_number(n);
+        n = n - 1;
+    }
+}
+
+int main()
+{
+    ft_count_down(100);
+}
