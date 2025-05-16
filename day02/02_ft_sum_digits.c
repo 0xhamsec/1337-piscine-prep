21562053d5501007e48ffbd12368ac2d3f8fe802
Th3Hacker <hamza.annane.official@gmail.com>
2025-05-16 20:08:56 +0100

    sum digits  function

--- day02/02_ft_sum_digits.c
+++ day02/02_ft_sum_digits.c
@@ -13,3 +13,28 @@
 /*     Example: 123 => 1 + 2 + 3 = 6                                          */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_sum_digits(int number)
+{
+    int sum = 0;
+    int digit;
+    
+    while (number != 0)
+    {
+        digit = number % 10;
+        sum = sum + digit;
+        number = number / 10;
+    }
+
+    return sum;
+}
+
+int main()
+{
+    int number = 123;
+    int res = ft_sum_digits(number);
+    printf("%d\n",res);
+
+}
