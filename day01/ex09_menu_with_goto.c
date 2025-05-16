98f557597266ffeb1d1271a5c39740b63a123aab
Th3Hacker <hamza.annane.official@gmail.com>
2025-05-16 12:58:22 +0100

    .

--- day01/ex09_menu_with_goto.c
+++ day01/ex09_menu_with_goto.c
@@ -10,10 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /*
-** Show a menu with options like:
+** A simple menu program using goto
+** Options:
 ** 1. Greet
 ** 2. Show date
 ** 3. Exit
-** Use goto to loop back to menu after each option.
 */
+
+int main(void)
+{
+    int choice;
+
+menu:
+    printf("\n=== MENU ===\n");
+    printf("1. Greet\n");
+    printf("2. Show date\n");
+    printf("3. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+    {
+        printf("Hello! Have a wonderful day!\n");
+        goto menu;
+    }
+    else if (choice == 2)
+    {
+        printf("Showing current date and time:\n");
+        system("date"); // Calls system command to show date/time
+        goto menu;
+    }
+    else if (choice == 3)
+    {
+        printf("Exiting the program. Goodbye!\n");
+        return (0);
+    }
+    else
+    {
+        printf("Invalid choice. Please try again.\n");
+        goto menu;
+    }
+}
