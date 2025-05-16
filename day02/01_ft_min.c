0cba3ca545746d622c1eccd20c73763777671494
Th3Hacker <hamza.annane.official@gmail.com>
2025-05-16 19:26:39 +0100

    min int  function

--- day02/01_ft_min.c
+++ day02/01_ft_min.c
@@ -12,3 +12,20 @@
 /*     This function returns the smaller of two integer values.              */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_min(int n1, int n2)
+{
+    if (n1 > n2)
+        return n2;
+    return n1;
+}
+
+int main()
+{
+    int a = 13;
+    int b = 37;
+    int res = ft_min(a,b);
+    printf("%d\n",res);
+}
