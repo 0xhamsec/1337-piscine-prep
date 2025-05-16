e1c0f10c886de4ad5c44f718c1f101963ae1a76b
Th3Hacker <hamza.annane.official@gmail.com>
2025-05-16 15:07:20 +0100

    day02 1337 piscine prep

---
+++ day02/09_ft_is_palindrome.c
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_palindrome.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: 0xhamsec <0xhamsec@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 00:00:00 by 0xhamsec          #+#    #+#             */
+/*   Updated: 2025/05/15 00:00:00 by 0xhamsec         ###   ########.fr       */
+/*                                                                            */
+/*   Description:                                                             */
+/*     This function checks whether an integer is a palindrome or not.       */
+/*     Returns 1 if it is, 0 otherwise.                                       */
+/*                                                                            */
+/* ************************************************************************** */
