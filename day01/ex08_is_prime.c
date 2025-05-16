b13e05c7b48d8739e48b4afa61442d2434d6eece
Th3Hacker <hamza.annane.official@gmail.com>
2025-05-16 12:49:42 +0100

    is prime number

--- day01/ex08_is_prime.c
+++ day01/ex08_is_prime.c
@@ -13,3 +13,34 @@
 /*
 ** Ask user for a number. Use a while loop to check if it's prime.
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int count = 2;
+    int number;
+    int is_prime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d",&number);
+
+    while (count * count <= number)
+    {
+        if (number % count == 0)
+        {
+            is_prime = 0;
+            break;
+        }
+        count++;
+    }
+
+    if (is_prime)
+    {
+        printf("the %d is a Prime number\n",number);
+    }
+    else
+        printf("the %d is NOT a Prime number\n",number);
+
+    return 0;
+}
