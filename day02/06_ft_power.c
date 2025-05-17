ece7e40e387cabf689d5d7decb685385bd45fefa
Th3Hacker <hamza.annane.official@gmail.com>
2025-05-17 01:24:40 +0100

    power  function

--- day02/06_ft_power.c
+++ day02/06_ft_power.c
@@ -13,3 +13,28 @@
 /*     Example: ft_power(2, 3) => 8                                           */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_power(int number, int power)
+{
+    int res = 1;
+    if (power < 0)
+        return 0;
+    while (power > 0)
+    {
+        res =  res * number;
+        power = power - 1;
+    }
+    return res;
+}
+
+int main(void)
+{
+    printf("2^3 = %d\n", ft_power(2, 3)); // 8
+    printf("5^0 = %d\n", ft_power(5, 0)); // 1
+    printf("3^4 = %d\n", ft_power(3, 4)); // 81
+    printf("2^-2 = %d\n", ft_power(2, -2)); // 0 (not supported)
+
+    return 0;
+}
